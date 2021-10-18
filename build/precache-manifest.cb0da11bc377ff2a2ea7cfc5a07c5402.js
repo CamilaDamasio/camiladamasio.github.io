0c6521240c773d8ad5b3eeec3ce86fffe74f26c4
CamilaDamasio <camiladamasioef@hotmail.com>
2021-10-18 14:32:56 -0300

    update

---
+++ build/precache-manifest.cb0da11bc377ff2a2ea7cfc5a07c5402.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c96804b7b44245f7a1a7492f7cda0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "492c06704901ca748041",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "c0a361f75befc0b08378",
+    "url": "/static/css/main.1af76343.chunk.css"
+  },
+  {
+    "revision": "492c06704901ca748041",
+    "url": "/static/js/2.be326209.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.be326209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a361f75befc0b08378",
+    "url": "/static/js/main.7c4cc2e2.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  },
+  {
+    "revision": "348d8b05a780c9040ed404919c290c86",
+    "url": "/static/media/fundo-contato.348d8b05.png"
+  },
+  {
+    "revision": "1157c989c0d597bf12cb2f4e08ccc28c",
+    "url": "/static/media/fundo-home.1157c989.jpg"
+  },
+  {
+    "revision": "cbd07b3c435ce31cce271f24e6e074f8",
+    "url": "/static/media/fundo-projects.cbd07b3c.png"
+  },
+  {
+    "revision": "c12fe601481d172352528228a887c4a0",
+    "url": "/static/media/my-photo.c12fe601.png"
+  }
+]);
